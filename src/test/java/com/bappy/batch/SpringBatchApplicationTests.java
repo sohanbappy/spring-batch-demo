cc9d6936aa084f96fce41255038ea0d5fa919f00
SohanBappy <sohanbappy@gmail.com>
2021-10-27 02:43:58 +0600

    Project initialized

---
+++ src/test/java/com/bappy/batch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bappy.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
