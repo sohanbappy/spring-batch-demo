685a1ab54951c63c9b281990105e3d98fbd7f2da
SohanBappy <sohanbappy@gmail.com>
2021-10-27 04:03:33 +0600

    DateTime added for User

--- src/main/java/com/bappy/batch/helper/BatchProcessor.java
+++ src/main/java/com/bappy/batch/helper/BatchProcessor.java
@@ -4,6 +4,8 @@ import com.bappy.batch.entity.User;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 
 @Component
 public class BatchProcessor implements ItemProcessor<User, User> {
@@ -11,6 +13,7 @@ public class BatchProcessor implements ItemProcessor<User, User> {
     @Override
     public User process(User user) {
         //process can be done upon user (if any)
+        user.setCreatedAt(new Date());
         return user;
     }
 
